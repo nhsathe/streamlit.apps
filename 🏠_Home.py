90675737439eec21cf6cbc6dab4b156f0b6ac31d
nhsathe <112503941+nhsathe@users.noreply.github.com>
2024-09-27 20:56:53 -0400

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -6,3 +6,4 @@ st.set_page_config(
 )
 
 st.write("# Welcome to Nishank's projects on Streamlit! 👋")
+st.page_link("pages/1_🏭_Facility_Location_Selection.py", label="Facility Location Selection", icon="🏭")
