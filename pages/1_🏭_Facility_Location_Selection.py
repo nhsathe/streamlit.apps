3c3d03d12e19da02b3517493ce7d034089eb280d
nhsathe <112503941+nhsathe@users.noreply.github.com>
2024-10-06 15:36:12 -0400

    Update 1_🏭_Facility_Location_Selection.py

--- pages/1_🏭_Facility_Location_Selection.py
+++ pages/1_🏭_Facility_Location_Selection.py
@@ -165,8 +165,8 @@ def main():
     # Run the model
     if st.button("Run Model"):
         model = create_model(dist_mat, population, P, objective_type, r_max)
-        #solver = pyo.SolverFactory('cbc')
-        solver = pyo.SolverFactory('highs')
+        solver = pyo.SolverFactory('cbc')
+        #solver = pyo.SolverFactory('highs')
         
 
         # Ensure the solver is correctly applied
